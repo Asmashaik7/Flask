eec2521baa0ca0199288ebaea3eeda312360906d
Asmashaik7 <asmashaik.nov11@gmail.com>
2025-06-20 16:36:59 +0530

    Add files via upload

--- secondapp.py
+++ secondapp.py
@@ -19,10 +19,6 @@ Below are some cute pictures of kittens...
 <img src="https://www.warrenphotographic.co.uk/photography/bigs/10113-Three-cute-kittens-white-background.jpg" id="img" style="width: 496px; height: 355px;">
 
 <p>Cat intelligence refers to a cat’s ability to solve problems, adapt to its environment, learn new behaviors, and communicate its needs. Structurally, a cat’s brain shares similarities with the human brain,[1] containing around 250 million neurons in the cerebral cortex, which is responsible for complex processing.[2] Cats display neuroplasticity, allowing their brains to reorganize based on experiences. They have well-developed memory retaining information for a decade or longer. These memories are often intertwined with emotions, allowing cats to recall both positive and negative experiences associated with specific places.[3] While they excel in observational learning and problem-solving, studies conclude that they struggle with understanding cause-and-effect relationships in the same way that humans do.</p1>
-<p>Origin and history of cats
-After the nonavian dinosaurs became extinct, mammals became the dominant life forms. The first felinelike mammal, Proailurus, evolved about 30 million years ago. It is thought that all true cat species evolved from this small civetlike predator.
-
-Cats that resemble today’s felids first appeared in the early Pliocene Epoch (5.3 to 3.6 million years ago), and they have continued into present times with remarkably few changes. The original design of fang and claw, flexible backbone, muscular strength, and agility allowed felids to survive and adapt to the changes brought by each new era. Adaptations have occurred with changes in prey, but the basic body type has stayed the same.</p1>
 
 <a href="http://127.0.0.1:7777/aboutus">About us</a>
 
@@ -31,8 +27,6 @@ Cats that resemble today’s felids first appeared in the early Pliocene Epoch (
 <a href="http://127.0.0.1:7777/contactus">Contact us</a>
 
 <a href="https://www.gettyimages.in/photos/cute-little-kittens">Getty images</a>
-
-
 """
 
 # About us
@@ -45,7 +39,10 @@ Our original stories have been featured in prominent publishers such as people.c
 
 Thank you so much for stopping by. If you have a story that you'd like to share with us, you can write to us at contact@lovemeow.com . We are always happy to hear from you.
 
-More info:
+
+If you want to see cute kitten images on getty, here you go..<a href="https://www.gettyimages.in/photos/cute-little-kittens">Getty images</a>
+    
+<h4>More info:</h4>
 Mailing address: P.O. Box 11234 TG India
 
 Email: contact@lovemew.com</p>
@@ -56,7 +53,6 @@ Email: contact@lovemew.com</p>
 
     <a href="http://127.0.0.1:7777/contactus">Contact us</a>
 
-    <a href="https://www.gettyimages.in/photos/cute-little-kittens">Getty images</a>
     
     """
 
@@ -75,14 +71,18 @@ def service_page():
 
 </body>
 </html>
+<p><h3>Origin and history of cats</h3>
+After the nonavian dinosaurs became extinct, mammals became the dominant life forms. The first felinelike mammal, Proailurus, evolved about 30 million years ago. It is thought that all true cat species evolved from this small civetlike predator.
 
+Cats that resemble today’s felids first appeared in the early Pliocene Epoch (5.3 to 3.6 million years ago), and they have continued into present times with remarkably few changes. The original design of fang and claw, flexible backbone, muscular strength, and agility allowed felids to survive and adapt to the changes brought by each new era. Adaptations have occurred with changes in prey, but the basic body type has stayed the same.</p1>
+</p>
      <a href="http://127.0.0.1:7777/">Home</a>
 
     <a href="http://127.0.0.1:7777/aboutus">About Us</a>
 
     <a href="http://127.0.0.1:7777/contactus">Contact us</a>
 
-    <a href="https://www.gettyimages.in/photos/cute-little-kittens">Getty images</a>
+
     
     
     """
@@ -128,10 +128,14 @@ def contact_page():
 
     <a href="http://127.0.0.1:7777/services">Services</a>
 
-    <a href="https://www.gettyimages.in/photos/cute-little-kittens">Getty images</a>
+    
     
     
     """
+
+
+
+
 
 if __name__=='__main__':
     secondapp.run(debug=True, port=7777)
